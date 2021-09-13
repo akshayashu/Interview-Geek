7130fbf91e5bf12859f1c6f4f2797a021202a946
akshayashu <akshay.khanna0810@gmail.com>
2021-09-14 01:29:02 +0530

    implemented global vc

--- src/components/videocalling/VideoChat.js
+++ src/components/videocalling/VideoChat.js
@@ -1,38 +1,144 @@
 import './VideoChat.css';
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Peer from 'peerjs';
+import { Button, makeStyles } from '@material-ui/core';
 
+
+const useStyles = makeStyles({
+    Button: {
+        margin: 10
+    }
+});
+
+let constraints = {
+    video: true,
+    audio: true
+};
 
 function VideoChat() {
+    const videoRef = useRef(null);
+    const remoteRef = useRef(null);
+    const peerInstance = useRef(null);
 
-    const [userId, setUserId] = useState(null);
-    const [peerUser, setPeerUser] = useState(null);
-    const [localStream, setLocalStreeam] = useState("");
+    const [userId, setUserId] = useState("");
+    const [peerUser, setPeerUser] = useState("");
+    const [localStream, setLocalStream] = useState(null);
     const [remoteStream, setRemoteStream] = useState("");
+    const [videoAccess, setVideoAccess] = useState(true);
+    const [audioAccess, setAudioAccess] = useState(true);
+
+    const toggleVideo = (b) => {
+        console.log(b);
+        if (b) {
+            localStream.getVideoTracks()[0].enabled = true
+        } else {
+            localStream.getVideoTracks()[0].enabled = false
+        }
+        setVideoAccess(b);
+    }
+
+    const toggleAudio = (b) => {
+        console.log(b);
+        if (b) {
+            localStream.getAudioTracks()[0].enabled = true
+        } else {
+            localStream.getAudioTracks()[0].enabled = false
+        }
+        setAudioAccess(b);
+    }
 
     useEffect(() => {
-        if(userId == null){
-            return;
-        }
+        const peer = new Peer();
+        console.log(peer);
 
-        setPeerUser( new Peer(userId, {
-            secure: true,
-            host: '192.168.0.4',
-            port: 5000,
-            path: '/peerjs'
-        }))
-    }, [userId])
+        peer.on('open', (id) => {
+            console.log(id);
+            setUserId(id);
+        })
+        peerInstance.current = peer;
 
+        peer.on('call', (call) => {
+
+            navigator.getUserMedia(constraints, async (stream) => {
+                let video = videoRef.current
+                setLocalStream(stream)
+                video.srcObject = stream;
+                video.play();
+
+                console.log("Someone is calling " + call);
+                call.answer(stream)
+                call.on('stream', (remoteStr) => {
+                    let remoteVideo = remoteRef.current;
+                    setRemoteStream(remoteStr);
+                    remoteVideo.srcObject = remoteStr;
+                    remoteVideo.play();
+                })
+
+            })
+        })
+
+    }, []);
+
+    const startCall = () => {
+        navigator.getUserMedia(constraints, async (stream) => {
+
+            let video = videoRef.current
+            setLocalStream(stream)
+            video.srcObject = stream;
+            video.play();
+
+            const call = peerInstance.current.call(peerUser, stream);
+            call.on('stream', async (stream) => {
+                let remoteVideo = remoteRef.current;
+                setRemoteStream(stream);
+                remoteVideo.srcObject = stream;
+                remoteVideo.play();
+            })
+        })
+    }
+
+    const classes = useStyles();
 
     return (
-        <div class="container" >
-            <video className="primary-video" autoPlay id="local-video"></video>
-            <video className="secondary-video" autoPlay id="remote-video"></video>
-            <button onClick={() => {
-                setUserId(userId+"1");
-                console.log(userId)
-            }}>set id</button>
+        <div>
+            <div className="container" >
+                <video className="primary-video" autoPlay={true} id="local-video" ref={videoRef}></video>
+                <video className="secondary-video" autoPlay={true} id="remote-video" ref={remoteRef}></video>
+            </div>
+
+            <input
+                className="input-id"
+                placeholder="Enter Remote id"
+                onChange={(e) => {
+                    console.log(e.target.value);
+                    setPeerUser(e.target.value);
+                }}></input>
+            <Button
+                variant="contained"
+                onClick={() => {
+                    startCall()
+                }}>Call</Button>
+            <br />
+            <Button
+                variant="contained"
+                margin="10"
+                onClick={() => {
+                    toggleVideo(!videoAccess)
+                }}>Toggle video</Button>
+
+            <Button
+                InputProps={{
+                    classes: {
+                        input: classes.Button,
+                    },
+                }}
+                variant="contained"
+                margin="10"
+                onClick={() => {
+                    toggleAudio(!audioAccess)
+                }}>Toggle audio</Button>
         </div>
+
     );
 }
 export default VideoChat;
