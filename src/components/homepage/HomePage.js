a7ee2ad52cdcb32e69391e0db117f03cec4b7303
akshayashu <akshay.khanna0810@gmail.com>
2021-09-22 02:53:17 +0530

    glassmorphism ui

--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -4,23 +4,26 @@ import { NavLink } from 'react-router-dom';
 
 function HomePage() {
     return (
-        <div className="container">
-            <div className="top-Container">
-                <div className="header">
-                    <h1 className="logo">Interview Geek</h1>
-                    <ul className="page-route">
+        <div className="home-container">
+            <div className="home-top-container">
+                <div className="home-header">
+                    <h1 className="home-logo">Interview Geek</h1>
+                    <ul className="home-page-route">
 
                         <p>Home</p>
                         <p>About</p>
                         <p>
                             <NavLink
-                                style={{ textDecoration: 'none', color: 'black' }} to="/interview">Interview</NavLink>
+                                style={{ background: 'unset', textDecoration: 'none', color: 'black' }} to="/interview">Interview</NavLink>
                         </p>
                         <p>Login</p>
                     </ul>
                 </div>
-            </div>
+                <div className="home-intro">
+                    <img className="home-img"></img>
+                </div>
 
+            </div>
         </div>
     )
 }
